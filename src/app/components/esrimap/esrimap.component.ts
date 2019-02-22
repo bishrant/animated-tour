77dd07182969bb452f8ef1fabe07a6483e3049ab
Bishrant Adhikari <adhi_bishrant@hotmail.com>
2019-02-22 14:02:49 -0600

    animation with labels

--- src/app/components/esrimap/esrimap.component.ts
+++ src/app/components/esrimap/esrimap.component.ts
@@ -10,232 +10,229 @@ import * as vars from './variables';
 })
 export class EsrimapComponent implements OnInit {
   @ViewChild('mapViewNode') private mapViewEl: ElementRef;
-  private _zoom = 12;
+  private _zoom = 15;
   private _center: Array<number> = [-95.118420276135581, 31.072854453305986];
   private _basemap = 'streets';
+  mapView: __esri.MapView;
+  lineGraphic: __esri.Graphic;
+  ptGraphic: __esri.Graphic;
+  totalPaths = vars.line1.length;
+  public createGraphic;
+  public createLabels;
+  public countPointsPerLine;
+  public createPolyline;
+  public that;
+  public lastBearing = 90;
+  delay = 100; // milli seconds delay
+  legNumber = 1; // first leg
+  legHop = 0;
+  distanceLat = 0.005; // 0.0005
+  allPtGraphics = [];
+  allPtArray = [];
+  public setTimeOutAnimation;
+  _polyline: __esri.Polyline;
+  ptLayer: __esri.GraphicsLayer;
+  lineLayer: __esri.GraphicsLayer;
+  labelsLayer: __esri.GraphicsLayer;
+  map: __esri.Map;
   constructor() { }
+
+  animateRoute = () => {
+    console.log(this.lineGraphic);
+  }
+
+  public addLabel(legNumber, c) {
+    const newLabel = this.createLabels(vars.labels[legNumber], c.geometry);
+     this.labelsLayer.add(newLabel);
+     this.labelsLayer.add(this.createGraphic(vars.stopSymbol, c.geometry));
+  }
+
+  public getTotalHopsMaster(id) {
+    const count = this.countPointsPerLine(vars.line1[id], 50);
+    return count;
+  }
+
+  public getTotalHops(id) {
+    // return 200;
+    // const count = countPointsPerLine(vars.line1[id ], 1000);
+    // return count;
+    return this.allPtArray[id].length;
+  }
+
+  public prepareForAnimation(polyline) {
+    // loop for the all paths inside that polyline
+    for (let pthId = 0; pthId < polyline.paths.length; pthId++) {
+      // get total hops per individual path sections
+      const countPerleg = this.getTotalHopsMaster(pthId);
+      // console.log(countPerleg);
+      // now get all those points
+      const ptArraySection = [];
+      const ptGraphicsArraySection = [];
+      // add first point
+      const ptGraphic1 = this.createGraphic(vars.markerSymbol,
+        {
+          type: 'point',
+          longitude: polyline.paths[pthId][0][0],
+          latitude: polyline.paths[pthId][0][1],
+          spatialReference: this.mapView.spatialReference
+        });
+
+      ptGraphicsArraySection.push(ptGraphic1);
+      ptArraySection.push([polyline.paths[pthId][0][0], polyline.paths[pthId][0][1]]);
+
+      for (let hop = 0; hop <= countPerleg; hop++) {
+        const c = vars.getPointAlongLine(polyline, (hop + 1) * this.distanceLat, pthId);
+        if (c !== null) {
+          this.ptGraphic = this.createGraphic(vars.markerSymbol,
+            {
+              type: 'point',
+              longitude: c[0],
+              latitude: c[1],
+              spatialReference: this.mapView.spatialReference
+            });
+          ptGraphicsArraySection.push(this.ptGraphic);
+          ptArraySection.push([c[0], c[1]]);
+        } else {
+          hop = countPerleg + 5;
+        }
+      }
+      // also add the ending point
+      const _totalPt = polyline.paths[pthId].length;
+      const ptGraphic2 = this.createGraphic(vars.markerSymbol,
+        {
+          type: 'point',
+          longitude: polyline.paths[pthId][_totalPt - 1][0],
+          latitude: polyline.paths[pthId][_totalPt - 1][1],
+          spatialReference: this.mapView.spatialReference
+        });
+
+      ptGraphicsArraySection.push(ptGraphic2);
+      ptArraySection.push([polyline.paths[pthId][_totalPt - 1][0], polyline.paths[pthId][_totalPt - 1][1]]);
+      this.allPtArray.push(ptArraySection);
+      this.allPtGraphics.push(ptGraphicsArraySection);
+    }
+    this.addLabel(this.legNumber - 1, this.allPtGraphics[this.legNumber - 1][this.legHop]);
+    console.log(this.allPtArray);
+    console.log(polyline.extent);
+    const e = polyline.extent.clone();
+    this.mapView.extent = e.expand(1.5);
+    this.animateAll();
+  }
+
+  public animateAll() {
+    this.legHop = this.legHop + 1;
+    let newpaths;
+    if (this.legHop === 0) {
+      newpaths = [this.allPtArray[this.legNumber - 1].slice(this.legHop, this.legHop + 1)];
+    } else if (this.legHop >= this.getTotalHops(this.legNumber - 1)) {
+      newpaths = [this.allPtArray[this.legNumber - 1].slice(this.legHop - 1, this.legHop)];
+    } else {
+      newpaths = [this.allPtArray[this.legNumber - 1].slice(this.legHop - 1, this.legHop + 1)];
+    }
+    this.ptLayer.removeAll();
+    const line1 = this.createPolyline(newpaths, 4326);
+    const _graphic = this.allPtGraphics[this.legNumber - 1][this.legHop - 1];
+    this.lastBearing = (vars.bearing(newpaths) === -9999) ? vars.bearing(newpaths) : this.lastBearing;
+    _graphic.symbol.angle = vars.bearing(newpaths);
+    const lineGraphic1 = this.createGraphic(vars.highLightedLineSymbol, line1);
+    this.lineLayer.add(lineGraphic1);
+
+      this.ptLayer.add(_graphic);
+    /* animation logic */
+    if (this.legNumber < this.totalPaths) {
+      if (this.legHop > (this.getTotalHops(this.legNumber - 1) - 1) && this.legNumber <= this.totalPaths) {
+        this.addLabel(this.legNumber, this.allPtGraphics[this.legNumber - 1][this.legHop - 1]);
+        this.legNumber++;
+        this.legHop = 0;
+        this.setTimeOutAnimation = setTimeout(() => { this.animateAll(); }, this.delay);
+      } else if (this.legHop >= this.getTotalHops(this.legNumber - 1) && this.legNumber > this.totalPaths) {
+        return;
+      } else {
+        // tslint:disable-next-line:max-line-length
+        this.setTimeOutAnimation = setTimeout(() => {  this.animateAll(); }, (this.legHop === 1 && this.legNumber === 1 ) ? 200 : this.delay);
+      }
+    } else if (this.legHop < this.getTotalHops(this.legNumber - 1)) {
+      this.setTimeOutAnimation = setTimeout(() => { this.animateAll(); }, this.delay);
+    } else {
+      // this.ptLayer.removeAll();
+      this.addLabel(this.legNumber, this.allPtGraphics[this.legNumber - 1][this.legHop - 1]);
+      console.log('finally done');
+    }
+  }
 
   async initializeMap() {
 
-    const [EsriMap, EsriMapView, GraphicsLayer, Graphic, Polyline, Point, geometryEngine] = await loadModules([
-      'esri/Map', 'esri/views/MapView',  'esri/layers/GraphicsLayer',   'esri/Graphic',
-      'esri/geometry/Polyline',  'esri/geometry/Point',    'esri/geometry/geometryEngine',
+    const [EsriMap, EsriMapView, GraphicsLayer, Graphic, TextSymbol, Polyline, Point, geometryEngine] = await loadModules([
+      'esri/Map', 'esri/views/MapView', 'esri/layers/GraphicsLayer', 'esri/Graphic', 'esri/symbols/TextSymbol',
+      'esri/geometry/Polyline', 'esri/geometry/Point', 'esri/geometry/geometryEngine',
       'dojo/domReady'
     ]);
 
-    const map: esri.Map = new EsriMap({ basemap: this._basemap });
-
+    this.map = new EsriMap({ basemap: this._basemap });
     // Initialize the MapView
     const mapViewProperties: esri.MapViewProperties = {
       container: this.mapViewEl.nativeElement,
       center: this._center,
       zoom: this._zoom,
-      map: map
+      map: this.map
     };
 
-    const mapView = new EsriMapView(mapViewProperties);
+    this.mapView = new EsriMapView(mapViewProperties);
     const line = new Polyline({
       paths: vars.line1,
       spatialReference: { wkid: 4326 }
     });
 
-    const lineGraphic = new Graphic({
+    this.lineGraphic = new Graphic({
       geometry: line,
       symbol: vars.lineGraphicSymbol
     });
 
-    const lineLayer = new GraphicsLayer();
-    const ptLayer = new GraphicsLayer();
-    const labelsLayer = new GraphicsLayer();
-lineLayer.add(lineGraphic);
-    map.addMany([ptLayer, lineLayer, labelsLayer]);
+    this.createGraphic = (s, g) => {
+      return new Graphic({ geometry: g, symbol: s });
+    };
 
-
-    // function animate() {
-    //   let i = 0;
-    //   const totalStops = ptGraphicsArray.length;
-    //   function f() {
-    //     ptLayer.removeAll();
-    //     const newpaths = [ptArray.slice(i, i + 2)];
-    //     const line1 = new Polyline({
-    //       paths: newpaths,
-    //       spatialReference: { wkid: 4326 }
-    //     });
-    //     const lineGraphic1 = new Graphic({
-    //       geometry: line1,
-    //       symbol: vars.highLightedLineSymbol
-    //     });
-    //     lineLayer.add(lineGraphic1);
-    //     ptLayer.add(ptGraphicsArray[i]);
-    //     i++;
-    //     if (i < totalStops) {
-    //       setTimeout(f, 1);
-    //     } else {
-    //       console.log('done');
-    //     }
-    //   }
-    //   f();
-    // }
-
-    // need to count total points between nodes
-    // console.log(geometryEngine.geodesicLength(line));
-    // const ptGraphicsArray = [];
-    // const ptArray = [];
-    // for (let ii = 0.005; ii < 25; ii += 0.0008) {
-    //   const c = vars.getPointAlongLine(line, ii, 0);
-    //   if (c !== null) {
-    //     const ptGraphic = new Graphic({
-    //       symbol: vars.markerSymbol,
-    //       geometry: {
-    //         type: 'point',
-    //         longitude: c[0],
-    //         latitude: c[1],
-    //       }
-    //     });
-    //     ptGraphicsArray.push(ptGraphic);
-    //     ptArray.push([c[0], c[1]]);
-    //   } else {
-    //     break;
-    //   }
-    // }
-   // ptLayer.addMany(ptGraphicsArray);
-
-   // animate();
-
-   const totalPaths = vars.line1.length;
-   let legNumber = 1; // first leg
-   let legHop = 0;
-   const distanceLat = 0.0005;
-   const allPtGraphics = [];
-   const allPtArray = [];
-
-   function prepareForAnimation(polyline) {
-    //  console.log(polyline);
-     // loop for the all paths inside that polyline
-     for (let pthId = 0; pthId < polyline.paths.length; pthId++) {
-        // get total hops per individual path sections
-        const countPerleg = getTotalHopsMaster(pthId);
-        console.log(countPerleg);
-        // now get all those points
-        const ptArraySection = [];
-        const ptGraphicsArraySection = [];
-        for (let hop = 0; hop <= countPerleg ; hop++) {
-           const c = vars.getPointAlongLine(polyline, (hop + 1 ) * distanceLat, pthId);
-          if (c !== null) {
-            const ptGraphic = new Graphic({
-              symbol: vars.markerSymbol,
-              geometry: {
-                type: 'point',
-                longitude: c[0],
-                latitude: c[1],
-                spatialReference: {wkid: 4326}
-              }
-            });
-            ptGraphicsArraySection.push(ptGraphic);
-            ptArraySection.push([c[0], c[1]]);
-            // console.log(c);
-          } else {
-            hop = countPerleg + 5;
-          }
-        }
-        console.log(pthId, ptArraySection);
-        allPtArray.push(ptArraySection);
-        allPtGraphics.push(ptGraphicsArraySection);
-     }
-    // labelsLayer.addMany(allPtGraphics[0]);
-    // console.log(allPtArray);
-    // const _line1 = new Polyline({
-    //   paths: allPtArray,
-    //   spatialReference: { wkid: 4326 }
-    // });
-    // const _lineGraphic1 = new Graphic({
-    //   geometry: _line1,
-    //   symbol: vars.highLightedLineSymbol
-    // });
-    // lineLayer.add(_lineGraphic1);
-     animateAll();
-   }
-
-
-    function getTotalHops(id) {
-      // return 200;
-      // const count = countPointsPerLine(vars.line1[id ], 1000);
-      // return count;
-      return allPtArray[id].length;
-    }
-
-   function getTotalHopsMaster(id) {
-        const count = countPointsPerLine(vars.line1[id ], 10);
-      return count;
-    }
-
-    function get(n, h) {
-      return vars.getPointAlongLine(line, 0.00005 * (legHop), n);
-    }
-
-function addLabel(legNumber) {
-  const newLabel = '';
-//  labelsLayer.add(null);
-}
-
-    function animateAll() {
-      legHop++;
-      // console.log('master leg', legNumber, legHop);
-      // animation logic
-      ptLayer.removeAll();
-      // console.log(allPtArray[legNumber - 1]);
-      const newpaths = [allPtArray[legNumber - 1].slice(legHop - 1, legHop + 1)];
-      // console.log(newpaths);
-      const line1 = new Polyline({
-        paths: newpaths,
-        spatialReference: { wkid: 4326 }
+    this.createPolyline = (_paths, _wkid) => {
+      return new Polyline({
+        paths: _paths,
+        spatialReference: { wkid: _wkid }
       });
-      const lineGraphic1 = new Graphic({
-        geometry: line1,
-        symbol: vars.highLightedLineSymbol
-      });
-      lineLayer.add(lineGraphic1);
-      map.add(lineLayer);
-      console.log(allPtGraphics[legNumber - 1][legHop - 1], legHop, getTotalHops(legNumber - 1), legNumber);
-      ptLayer.add(allPtGraphics[legNumber - 1][legHop - 1]);
-    //  console.log(allPtGraphics[0]);
-      /* animation logic */
-      if (legNumber < totalPaths) {
-        if (legHop > (getTotalHops(legNumber - 1) - 1 ) && legNumber <= totalPaths) {
-          legNumber++;
-          console.log('leg number increased');
-          legHop = 0;
-          ptLayer.removeAll();
-          addLabel(legNumber - 1);
-          setTimeout(animateAll, 3);
-        } else if (legHop >= getTotalHops(legNumber - 1) && legNumber > totalPaths) {
-          return;
-        } else {
-          setTimeout(animateAll, 3);
-        }
-      } else if (legHop < getTotalHops(legNumber - 1)) {
-        setTimeout(animateAll, 3);
-      }
-    }
-    const _polyline = new Polyline({
-      paths: vars.line1,
-      spatialReference: { wkid: 4326 }
-    });
-    prepareForAnimation(_polyline);
+    };
 
-    function countPointsPerLine(polylinePath, distance) {
+    this.countPointsPerLine = (polylinePath, distance) => {
       const pl = new Polyline({
         paths: polylinePath,
         spatialReference: { wkid: 4326 }
       });
-      console.log(geometryEngine.geodesicLength(pl, 'meters'), distance);
       return Math.round(geometryEngine.geodesicLength(pl, 'meters') / distance);
-    }
-    return mapView;
+    };
+
+    this.ptGraphic = new Graphic({ geometry: null, symbol: null });
+    this.lineLayer = new GraphicsLayer();
+    this.ptLayer = new GraphicsLayer();
+    this.labelsLayer = new GraphicsLayer();
+    this.lineLayer.add(this.lineGraphic);
+    this.map.addMany([this.ptLayer, this.lineLayer, this.labelsLayer]);
+
+    this._polyline = new Polyline({
+      paths: vars.line1,
+      spatialReference: { wkid: 4326 }
+    });
+
+
+    this.createLabels = (label, geometry) => {
+      const _lblSymbol = vars.labelSymbol;
+      _lblSymbol.text = label;
+      return this.createGraphic(_lblSymbol, geometry);
+    };
+    return this.mapView;
   }
 
   ngOnInit() {
     this.initializeMap();
+  }
+  public stopAnimation() {
+    clearTimeout(this.setTimeOutAnimation);
   }
 
 }
